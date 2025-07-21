826d597dfc4d3975798ccc42ca3c567c779923e4
christian medina <chrismedina3112@gmail.com>
2025-07-21 11:49:01 -0500

    Redirigir / hacia /api/

--- backend/urls.py
+++ backend/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    
-    # Autenticación con JWT (usa backend/urls_auth.py)
+
+    # Autenticación con JWT
     path('api/token/', include('backend.urls_auth')),
 
-    # Rutas de usuarios: registro, conductor, pasajero, estado, etc.
+    # Rutas de usuarios
     path('api/', include('users.urls')),
-]
 
+    # Redirección de / hacia /api/
+    re_path(r'^$', RedirectView.as_view(url='/api/', permanent=False)),
+]
