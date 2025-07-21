cbc506da977e2f4ffc997f8a7706cf9cbc78bb67
christian medina <chrismedina3112@gmail.com>
2025-07-21 12:58:34 -0500

    Agregando Swagger UI y Redoc

--- backend/urls.py
+++ backend/urls.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+# Swagger
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
 # Vista base para / y /api/
 def api_root_view(request):
     return JsonResponse({
@@ -12,9 +17,22 @@ def api_root_view(request):
             "/api/register/",
             "/api/register/driver/",
             "/api/register/passenger/",
-            "/api/driver/status/"
+            "/api/driver/status/",
+            "/docs/",
+            "/redoc/"
         ]
     })
+
+# Configuración de Swagger
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MoveNet API",
+        default_version='v1',
+        description="Documentación de la API de MoveNet",
+    ),
+    public=True,
+    permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
     path('', api_root_view),  # Para que / muestre algo
@@ -23,4 +41,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', include('backend.urls_auth')),
     path('api/', include('users.urls')),
+
+    # Swagger UI
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
